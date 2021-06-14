191bbce1b67f9ebfa89edf3a143bc97b463c3950
Leonardo Yoshii <leo_yoshii@outlook.com>
2021-06-14 06:47:42 -0300

    resolving bug pg:ssl

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,6 @@ console.log('DATABASE_URL:', process.env.DATABASE_URL);
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  ssl: true,
   entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
   migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
   cli: {
